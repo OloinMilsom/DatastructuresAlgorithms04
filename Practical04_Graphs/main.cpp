9710ccdc812132462a1ea62a611c7879f4aeca47
OloinMilsom <keporagebora@gmail.com>
2016-01-22 13:34:45 +0000

    q2 started

---
+++ Practical04_Graphs/main.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+#include <fstream>
+
+#include "Graph.h"
+
+using namespace std;
+
+
+typedef GraphArc<int, int> Arc;
+typedef GraphNode<int, int> Node;
+
+void visit( Node * pNode ) {
+	cout << "Visiting: " << pNode->data() << endl;
+}
+
+int main(int argc, char *argv[]) {
+   
+	Graph<int, int> graph(25);
+	
+	int c = 0;
+	int i = 0;
+	ifstream myfile;
+	myfile.open ("Q2Nodes.txt");
+
+	while ( myfile >> c ) {
+		graph.addNode(c, i++);
+	}
+
+	myfile.close();
+	myfile.open("Q2Arcs.txt");
+
+	int from, to, weight;
+	while ( myfile >> from >> to >> weight ) {
+		graph.addArc(from, to, weight);
+	}
+    myfile.close();
+
+	// Now traverse the graph.
+	graph.breadthFirst( graph.nodeArray()[0], visit );
+
+	system("PAUSE");
+	
+}
